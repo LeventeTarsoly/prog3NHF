a5948f2d08714489339a2bfb6291662156039670
TarsolyLevente <tarsolyl@edu.bme.hu>
2023-12-05 01:13:41 +0100

    ÁÁÁÁÁÁÁÁÁÁÁÁÁÁÁÁÁÁ

--- src/tests/MemberModelTest.java
+++ src/tests/MemberModelTest.java
@@ -1,6 +1,5 @@
 package tests;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,10 +7,6 @@ class MemberModelTest {
 
     @BeforeEach
     void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
     }
 
     @Test
